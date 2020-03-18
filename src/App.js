e07cce5039ca0f53ad629ba23b845d698622efa7
AlanCS <AlanCS1010@gmail.com>
2020-03-18 16:35:51 -0700

    Added/changed the accompanying text.

--- src/App.js
+++ src/App.js
@@ -20,10 +20,27 @@ function App() {
 
     return (
         <div className="App">
-            <p> A modified (more dynamic) version of the App in the
-                <a href="https://reactjs.org/docs/integrating-with-other-libraries.html#integrating-with-jquery-chosen-plugin"> React
-                    Documents on Integration with jQuery Plugins </a>
+            <h1>Integrating jQuery Plugins into a Modern React App</h1>
+            <p> This is a modified (more dynamic) version of the App in the React
+                Documents on
+                <a href="https://reactjs.org/docs/integrating-with-other-libraries.html#integrating-with-jquery-chosen-plugin"> Integration with jQuery Plugins </a>.
+                The purpose of this app is to display a picklist of countries using the jQuery&nbsp;
+                <a href="https://harvesthq.github.io/chosen/"><b>Chosen</b></a> plugin.
             </p>
+            <ul>
+                <li>
+                    Clicking the <b>Hide Component</b> button will cause the component to be unmounted, thereby calling the jQuery <b>off</b> and the chosen <b>destroy</b> events from the React componentWillUnmount lifecycle method . Clicking <b>Show Component</b> will mount the component again and the chosen plugin will re initialize once more with the help of the React componentDidMount lifecycle method.
+                </li>
+                <li>
+                    Clicking the <b>Disable Chosen</b> button will cause the Chosen plugin to be destroyed and so the picklist of countries will default to it's native look and feel. Clicking the <b>Enable Chosen</b> will again cause the Picklist to be managed by the Chosen plugin. This is done by creating a ref on the Chosen component, thereby allowing the parent ChosenWrapper component to invoke methods on the Chosen component.
+                </li>
+                <li>
+                    Clicking the <b>Show Hidden Countries</b> checkbox will cause the picklist of countries to change in the React component. React informs the Chosen component that the picklist has changed by triggering the jQuery <b>chosen:updated</b> event.
+                </li>
+                <li>
+                    When the user selects a different country from the picklist, the Chosen plugin invokes a handleChange handler, thereby informing the parent React ChosenWrapper component of the change.
+                </li>
+            </ul>
 
             { button }
             { Component }
